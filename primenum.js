cf1920c90c68e68b7c0854d44fe17e2b1d3dfb8b
muskanchetri24 <muskanchetri291@gmail.com>
2026-05-04 22:31:08 +0530

    primenumber

---
+++ primenum.js
@@ -0,0 +1,23 @@
+function isPrime(a)
+{
+    if(a<=1)
+    {
+        return false;
+    }
+    for(let i=2;i<=Math.sqrt(a);i++)
+    {
+        if(a%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+let a=17;
+if(isPrime(a))
+{
+    console.log("prime");
+}
+else{
+    console.log("not prime");
+}
